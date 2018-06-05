bcd6e3ec67e831ae72ff9c581bc7f56ebef965c0
james sewell <jamesjosephsewell@gmail.com>
2018-06-05 07:17:38 -0500

    boilerplate

---
+++ environment_vars_default.js
@@ -0,0 +1,9 @@
+// create a file in this directory called environment_vars.js
+// copy the below variables into it
+// replace the placeholder values with the appropriate values from your firebase config
+
+var API_KEY = "yourApiKey",
+AUTH_DOMAIN = "...",
+DATABASE_URL = "...",
+PROJECT_ID = "...",
+SENDER_ID = "..."
